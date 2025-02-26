61634730414e08aaaa9f6b6ffe6275f29fb48d9b
unknown <mai17005@byui.edu>
2025-02-26 09:57:54 -0700

    Completed prep 1 assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,3 +8,10 @@ class Program
         Console.WriteLine("Hello");
     }
 }
+
+/*
+Console.WriteLine() prints, then moves to next line
+Console.Write() prints and doesn't move to next line
+Console.ReadLine() returns what is written in the current line of the console, then moves to the next line
+Console.Read() returns what is written in the current line of the console
+*/
