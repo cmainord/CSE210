08126499e98a38d59eece62467b13c1aceb4cd15
unknown <mai17005@byui.edu>
2025-02-27 09:36:00 -0700

    Completed prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        float gradePercent = float.Parse(Console.ReadLine());
+        
+        string gradeLetter = "";
+        bool pass = false;
+        
+        if (gradePercent <= 60)
+        {
+            gradeLetter = "F";
+        }
+        else if (gradePercent <= 70)
+        {
+            gradeLetter = "D";
+        }
+        else if (gradePercent <= 80)
+        {
+            pass = true;
+            gradeLetter = "C";
+        }
+        else if (gradePercent <=90)
+        {
+            pass = true;
+            gradeLetter = "B";
+        }
+        else
+        {
+            pass = true;
+            gradeLetter = "A";
+        }
+
+        Console.Write($"Your grade is {gradeLetter}. ");
+
+        if (pass)
+        {
+            Console.Write("You passed the course.");
+        }
+        else
+        {
+            Console.Write("You failed the course.");
+        }
     }
 }
